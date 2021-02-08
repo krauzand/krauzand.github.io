92139d06d13731ed321a88df439cd70a5c094320
krauzand <andris.krauze@gmail.com>
2021-02-08 19:06:03 +0200

    Added: celebration and warning animations

--- Laso/laso.js
+++ Laso/laso.js
@@ -4,6 +4,8 @@ function Laso(dictionary) {
     this.dictionary = dictionary;
 
     this.wordElement = document.getElementById('word');
+    this.imageElement = document.getElementById('word-image');
+    this.anagramElement = document.getElementById('anagram');
 
     this.words = null;
     this.wordArray = [];
@@ -45,25 +47,25 @@ function Laso(dictionary) {
             let wordImage = this.imagesPath + word.path;
             let anagramText = self.makeAnagram(wordText);
 
-            let image = document.getElementById('word-image');
-            image.innerHTML = `<img src="${wordImage}">`;
 
-            let anagram = document.getElementById('anagram');
-            anagram.dataset.word = wordText;
-            anagram.dataset.anagram = anagramText;
+            this.imageElement.innerHTML = `<img src="${wordImage}">`;
 
 
-            anagram.innerHTML = null;
+            this.anagramElement.dataset.word = wordText;
+            this.anagramElement.dataset.anagram = anagramText;
+
+            this.anagramElement.innerHTML = null;
             this.wordElement.innerHTML = null;
             this.wordArray = [];
 
             let i = 0;
             for (let letter of anagramText) {
-                this.wordElement.innerHTML += `<button class="btn m-1 btn-outline-primary" data-index="${i}" id="w-${i}" onclick="laso.putLetterBack(this)">&nbsp;</button>`;
-                anagram.innerHTML += `<button class="btn m-1 btn-outline-primary" id="a-${i}" data-index="${i}" onclick="laso.putLetter(this)">${letter}</button>`;
+                this.wordElement.innerHTML += `<button class="btn m-1 btn-outline-primary" data-index="${i}" id="w-${i}" onclick="laso.putLetterBack(this)" style="width:${80/anagramText.length+'%'}">&nbsp;</button>`;
+                this.anagramElement.innerHTML += `<button class="btn m-1 btn-outline-primary" id="a-${i}" data-index="${i}" onclick="laso.putLetter(this)" style="width:${80/anagramText.length+'%'}">${letter}</button>`;
                 this.wordArray.push('\u00a0');
                 i++;
             }
+
         }
 
     };
@@ -78,9 +80,22 @@ function Laso(dictionary) {
         letterElement.dataset.target = element.id;
         element.classList.add('invisible');
 
+        if (this.anagramElement.dataset.word === this.wordArray.join('')) {
+            this.celebrate();
+        }
+        else {
+            if (this.anagramElement.dataset.word.length === this.wordArray.filter(c => c !== '\u00a0').length) {
+                this.warning();
+            }
+        }
+
     };
 
     this.putLetterBack = function(element) {
+        if (element.textContent === '\u00a0') {
+            return;
+        }
+
         element.textContent = '\u00a0'; //no brake space &nbsp;
         this.wordArray[element.dataset.index] = '\u00a0';
         let anagramElement = document.getElementById(element.dataset.target);
@@ -101,8 +116,40 @@ function Laso(dictionary) {
 
             return array;
         }
-        return shuffleArray(word.split('')).join('');
-    }
+        let shuffle = shuffleArray(word.split('')).join('');
+
+        //try just one more time
+        if (shuffle === word) {
+            shuffle = shuffleArray(word.split('')).join('');
+        }
+
+        return shuffle;
+    };
+
+    this.celebrate = function() {
+
+        anime({
+            targets: '#' + this.wordElement.id + ' .btn',
+            scale: 1.1,
+            backgroundColor: 'rgb(40,167,69, .3)',
+            color: '#000',
+            direction: 'alternate',
+            delay: 10,
+        });
+    };
+
+    this.warning = function() {
+
+        anime({
+            targets: '#' + this.wordElement.id + ' .btn',
+            scale: 1.1,
+            backgroundColor: 'rgb(220,53,69, .3)',
+            color: '#000',
+            direction: 'alternate',
+            delay: 10,
+        });
+    };
+
 }
 
 let laso = new Laso('dict.json');
